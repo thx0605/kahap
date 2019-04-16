31b402496fc7b50d91963c5b28dfdd71677cf640
Ny0605 <s14113211@stu.edu.tw>
2019-04-16 17:20:14 +0800

    add MemberTable

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Member;
 use Illuminate\Http\Request;
 
 class MembersController extends Controller
 {
     //
+    public function index()
+    {
+        $datas = Member::all();
+        return view('members.index',['datas' => $datas]);
+    }
 }
