f678b206f052a8b39b30ed971d859a3e741db69b
Ny0605 <s14113211@stu.edu.tw>
2019-04-17 11:14:39 +0800

    composer remove

--- app/Member.php
+++ app/Member.php
@@ -3,11 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Scout\Searchable;
 class Member extends Model
 {
     //
-    use Searchable;
     protected $table = 'members';
     protected $fillable = ['name', 'gender', 'birthday', 'email', 'phone', 'address'];
 }
